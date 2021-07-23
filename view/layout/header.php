1de1fec30efe617fa1e6400dd56501b2688b0427
romelx23 <romelx23@gmail.com>
2021-07-23 18:40:23 -0500

    responsive al fin

--- view/layout/header.php
+++ view/layout/header.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-<html lang="es-PE">
-  <head>
-    <meta charset="UTC-5" />
-    <!-- <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-        <meta name='description' content=''>
-        <meta name='author' content=''>
-        <link rel='icon' href='/docs/4.0/assets/img/favicons/favicon.ico'> -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
       rel="stylesheet"
