14edf6d8284a5053f0869a4e05bcf8c900100764
Jorge Espinoza <Jespinoza@latamready.com>
2021-07-25 18:23:05 -0500

    cambios usuario

--- administracion/controllers/controlador_rutas.php
+++ administracion/controllers/controlador_rutas.php
@@ -10,22 +10,24 @@ if (isset($_SESSION["idcargo"])) {
     require_once "./view/layout/header.php";
 
     if (isset($_GET["vista"])) {
-        if ($_GET["vista"] == "cliente") {
+        if ($_GET["vista"] == "cliente" &&($_SESSION['idcargo'] == 1 || $_SESSION['idcargo'] == 2)) {
             require_once "./controllers/controlador_cliente.php";
 
         } elseif ($_GET["vista"] == "dispositivo") {
             require_once "./controllers/controlador_dispositivo.php";
-        } elseif ($_GET["vista"] == "atendidos") {
+        } elseif ($_GET["vista"] == "atendidos" &&($_SESSION['idcargo']==1 || $_SESSION['idcargo']==3)) {
             require_once "./view/vista_principal.html";
             require_once "./controllers//controlador_estado.php";
-        } elseif ($_GET["vista"] == "pedidos") {
+        } elseif ($_GET["vista"] == "pedidos" &&($_SESSION['idcargo']==1 || $_SESSION['idcargo']==3)) {
             require_once "./view/vista_principal.html";
             require_once "./controllers/controlador_atendidos.php";
-        } elseif ($_GET["vista"] == "atendidos2") {
+        } elseif ($_GET["vista"] == "atendidos2" &&($_SESSION['idcargo']==1 || $_SESSION['idcargo']==3)) {
             require_once "./view/vista_principal.html";
             require_once "./controllers/controlador_noatendidos.php";
-        } elseif ($_GET["vista"] == "usuarios") {
-            require_once "./controllers/controller_Trabajador.php";
+        } elseif ($_GET["vista"] == "usuarios" && ($_SESSION['idcargo']==1 || $_SESSION['idcargo']==3)) {
+            require_once "./controllers/controlador_gestionar_trabajador.php";
+            require_once "./view/vista_principal.html";
+            require_once("./view/Formulario_registro_trabajador.html");
         } else {
             require_once "./view/vista_principal.html";
         }
--- administracion/controllers/controller_Trabajador.php
+++ administracion/controllers/controller_Trabajador.php
@@ -30,77 +30,12 @@ if (isset($_POST["login"]) and isset($_POST["password"])) {
         }
 
     }
-} else if (isset($_POST["agregar"])) {
-    require_once "./view/vista_principal.html";
-    require_once "./view/Formulario_registro_trabajador.html";
-} else if (isset($_POST["verificar_usuario"])) {
+}  else if (isset($_POST["verificar_usuario"])) {
     $user = (isset($_POST["usuario"]) && !empty(trim($_POST["usuario"]))) ? $_POST["usuario"] : null;
     require_once __DIR__ . "/controladorGestionarUsuario.php";
     if (isset($user)) {
         $controlador = new controladorGestionarUsuario();
         $controlador->verificar_usuario(["usuario" => $user]);
-    }
-    exit;
-} else if (isset($_POST["guardar_usuario"])) {
-    require_once __DIR__ . "/controladorGestionarUsuario.php";
-    $cargo = (isset($_POST["cargo"]) && !empty(trim($_POST["cargo"]))) ? $_POST["cargo"] : null;
-    $nombre = (isset($_POST["nombre"]) && !empty(trim($_POST["nombre"]))) ? $_POST["nombre"] : null;
-    $apellido = (isset($_POST["apellido"]) && !empty(trim($_POST["apellido"]))) ? $_POST["apellido"] : null;
-    $contrasenia_1 = (isset($_POST["contrasenia_1"]) && !empty(trim($_POST["contrasenia_1"]))) ? $_POST["contrasenia_1"] : null;
-    $contrasenia_2 = (isset($_POST["contrasenia_2"]) && !empty(trim($_POST["contrasenia_2"]))) ? $_POST["contrasenia_2"] : null;
-    $user_nick = (isset($_POST["user_nick"]) && !empty(trim($_POST["user_nick"]))) ? $_POST["user_nick"] : null;
-    $email = (isset($_POST["email"]) && !empty(trim($_POST["email"]))) ? $_POST["email"] : null;
-
-    if (isset($cargo) && isset($nombre) && isset($apellido) && isset($contrasenia_1) && isset($contrasenia_2) && isset($user_nick) && isset($email)) {
-        
-        if ($contrasenia_1 !== $contrasenia_2) { // Si la contraseña no es igual. retonamos mensaje
-            session_start();
-            $_SESSION["mensaje"] = "La contraseña es incorrecta, por favor verifique";
-            $_SESSION["estado"] = true;
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-            exit();
-        }
-
-        $controlador = new controladorGestionarUsuario();
-        $controlador->call_insertar([ // Llama a la función insertar usuario interno
-            "idcargo" => (int) $cargo,
-            "nombre" => $nombre,
-            "apellido" => $apellido,
-            "pasword" => md5($contrasenia_1),
-            "usuario" => $user_nick,
-            "email" => $email,
-        ]);
-    } else {
-        session_start();
-        $_SESSION["estado"] = true;
-        $_SESSION["mensaje"] = "Completa los campos";
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-        exit();
-    }
-} else if (isset($_POST["contrasenia_usuario"])) {
-    require_once __DIR__ . "/controladorGestionarUsuario.php";
-    $usuario = (isset($_POST["usuario"]) && !empty(trim($_POST["usuario"]))) ? $_POST["usuario"] : null;
-    $contrasenia_1 = (isset($_POST["contrasenia_1"]) && !empty(trim($_POST["contrasenia_1"]))) ? $_POST["contrasenia_1"] : null;
-    $contrasenia_2 = (isset($_POST["contrasenia_2"]) && !empty(trim($_POST["contrasenia_2"]))) ? $_POST["contrasenia_2"] : null;
-
-    if (isset($contrasenia_1) && isset($contrasenia_2) && isset($usuario)) {
-
-        if ($contrasenia_1 !== $contrasenia_2) {
-            //session_start();
-            $_SESSION["mensaje"] = "La contraseña es incorrecta, por favor verifique";
-            $_SESSION["estado"] = true;
-            header('Location: ' . $_SERVER['HTTP_REFERER']);
-            exit();
-        }
-
-        $controlador = new controladorGestionarUsuario();
-        $controlador->call_actualizar([
-            "usuario" => $usuario,
-            "pasword" => md5($contrasenia_1),
-        ]);
-    } else {
-        include_once "../shared/formMensajeSistema.php";
-        (new formMensajeSistema())->accesso_denegado();
     }
     exit;
 } else {
